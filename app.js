0ddd2eae800777c0aa4bdd6aaa14413a7ba654bc
phanthaiprilly <phan.thaiprilly@gmail.com>
2026-05-19 09:28:16 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 const API_URL = "https://script.google.com/macros/s/AKfycby3vUE-cAAdfuXJj5mRvRpyz5qIkZ4G75s6hlimCjZ6AqbF6883MtSb5LlhVSxMCgxCvw/exec";
 
 async function apiCall(payload) {
+  const formData = new FormData();
+  formData.append("data", JSON.stringify(payload));
+
   try {
     const res = await fetch(API_URL, {
       method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(payload)
+      body: formData
     });
 
-    return await res.json();
+    const text = await res.text();
+    return JSON.parse(text);
+
   } catch (e) {
     return { ok: false, message: "Erreur réseau : " + e.message };
   }
 }
+
