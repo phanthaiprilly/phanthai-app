13a10d1ef8405ed26323b1029172d4ff35cfa4d6
phanthaiprilly <phan.thaiprilly@gmail.com>
2026-05-19 10:40:30 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbwTVluBTmtaxvq5ngW20GEs4lTSydMKjG0JaGLU_tK30RT-Xq-owvp2qfB7zzMgHuRw/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbxZ18JlxXMzjT1Y-s8b1M2lgYSU4ZIknPv6RTIYkzN3uDV0SGL9gYgF2f9g0I82RXQY/exec";
 
 async function apiCall(payload) {
   const formData = new FormData();
